116f7a58f3b4e10958790f467055b71fc384f43a
lightfire224 <elias.taveras@columbia.edu>
2020-06-11 10:16:54 -0400

    Form onchange, changes corresponding state values

--- src/components/AddTransactionForm.js
+++ src/components/AddTransactionForm.js
@@ -1,22 +1,39 @@
 import React, { Component } from "react";
 
 class AddTransactionForm extends Component {
+  state = {
+    date: '',
+    description: '',
+    category: '',
+    amount: ''
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  // handleSubmit = {}
+
   render() {
     return (
       <div className="ui segment">
         <form className="ui form">
           <div className="inline fields">
-            <input type="date" name="date" />
-            <input type="text" name="description" placeholder="Description" />
-            <input type="text" name="category" placeholder="Category" />
+            <input type="date" value={this.state.date} onChange={this.handleChange} name="date" />
+            <input type="text" value={this.state.description} onChange={this.handleChange} name="description" placeholder="Description" />
+            <input type="text" value={this.state.category} onChange={this.handleChange} name="category" placeholder="Category" />
             <input
               type="number"
               name="amount"
+              value={this.state.amount}
+              onChange={this.handleChange}
               placeholder="Amount"
               step="0.01"
             />
           </div>
-          <button className="ui button" type="submit">
+          <button className="ui button" onClick={this.handleSubmit} type="submit">
             Add Transaction
           </button>
         </form>
