949c6b9f374fabdebb591494bf1160f10fe6d1a7
builtforsomethingbigger <builtforsomethingbigger@gmail.com>
2020-06-11 10:18:59 -0400

    first deliverable - CHECK

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -3,13 +3,30 @@ import TransactionsList from "./TransactionsList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
+const API = 'http://localhost:6001/transactions'
+
 class AccountContainer extends Component {
+
+  state = {
+    transactions: []
+  }
+
+  componentDidMount(){
+    this.renderTransactions()
+  }
+
+  renderTransactions = () => {
+    fetch(API)
+    .then(res => res.json())
+    .then(transactions => this.setState({ transactions }))
+  }
+
   render() {
     return (
       <div>
         <Search />
         <AddTransactionForm />
-        <TransactionsList />
+        <TransactionsList transactions={this.state.transactions} />
       </div>
     );
   }
--- src/components/AddTransactionForm.js
+++ src/components/AddTransactionForm.js
@@ -1,19 +1,49 @@
 import React, { Component } from "react";
 
+const API = 'http://localhost:6001/transactions'
+
 class AddTransactionForm extends Component {
+
+  state = {
+    date: '',
+    description: '',
+    category: '',
+    amount: null
+  }
+
+  onChangeHandler = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+  
+  postTransaction = () => {
+    fetch()
+  }
+
+  submitHandler = e => {
+    e.preventDefault()
+    // e.persist() /* hold in case you need */
+
+
+
+  }
+
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form className="ui form" onSubmit={this.submitHandler}>
           <div className="inline fields">
-            <input type="date" name="date" />
-            <input type="text" name="description" placeholder="Description" />
-            <input type="text" name="category" placeholder="Category" />
+            <input type="date" name="date" value={this.state.date} onChange={this.onChangeHandler} />
+            <input type="text" name="description" placeholder="Description" value={this.state.description} onChange={this.onChangeHandler} />
+            <input type="text" name="category" placeholder="Category" value={this.state.category} onChange={this.onChangeHandler} />
             <input
               type="number"
               name="amount"
               placeholder="Amount"
               step="0.01"
+              value={this.state.amount} 
+              onChange={this.onChangeHandler} 
             />
           </div>
           <button className="ui button" type="submit">
