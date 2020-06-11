d0e08bcfd13ef4e6aad4e6ef20539cc132c26e45
Vivica <Ma>
2020-06-11 09:51:42 -0400

    init

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import "../stylesheets/App.css";
 
 class App extends Component {
   render() {
+    
     return (
       <div className="ui raised segment">
         <div className="ui segment violet inverted">
