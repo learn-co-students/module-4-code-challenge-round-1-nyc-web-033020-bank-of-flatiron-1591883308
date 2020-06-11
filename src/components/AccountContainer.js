5505b6006e4fa8b5619ae413113f08c5c57da6ee
hensel01 <eliotrhenson@gmail.com>
2020-06-11 10:20:42 -0400

    1st commit

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -4,12 +4,28 @@ import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
 class AccountContainer extends Component {
+  state = {
+    transactions: [],
+    search: ""
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:6001/transactions')
+    .then(res => res.json())
+    .then(data => this.setState({transactions: data}))
+  }
+
+  handleSearch = (e) => {
+    this.setState({search: e.target.value})
+  }
+
   render() {
+    const searchTransactions = this.state.transactions.filter(t => t.description.toLowerCase().includes(this.state.search.toLowerCase()))  ///to lower case 
     return (
       <div>
-        <Search />
+        <Search handleSearch = {this.handleSearch} />
         <AddTransactionForm />
-        <TransactionsList />
+        <TransactionsList transactions = {searchTransactions} />
       </div>
     );
   }
--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -1,7 +1,8 @@
 import React from "react";
 import Transaction from "./Transaction";
 
-const TransactionsList = () => {
+const TransactionsList = (props) => {
+ 
   return (
     <table className="ui celled striped padded table">
       <tbody>
@@ -19,10 +20,11 @@ const TransactionsList = () => {
             <h3 className="ui center aligned header">Amount</h3>
           </th>
         </tr>
-        {/* render Transactions here */}
+        {props.transactions.map(transaction => <Transaction key= {transaction.id} {...transaction}/>)}
       </tbody>
     </table>
   );
 };
 
 export default TransactionsList;
+
