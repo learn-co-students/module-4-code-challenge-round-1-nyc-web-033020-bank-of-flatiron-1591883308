6234bd23e6686f962d3d7b3eb65f54e12a795038
elliott-king <8884571+elliott-king@users.noreply.github.com>
2020-06-11 10:21:59 -0400

    Form to add transaction

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -17,11 +17,25 @@ class AccountContainer extends Component {
       .then(txns => this.setState({txns: txns}))
   }
 
+  submitForm = (txn) => {
+    return fetch(API, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+      body: JSON.stringify(txn)
+    }).then(res => res.json())
+      .then(txn => {
+        this.setState(prevState => ({txns: [...prevState.txns, txn]}))
+      })
+  }
+
   render() {
     return (
       <div>
         <Search />
-        <AddTransactionForm />
+        <AddTransactionForm submitForm={this.submitForm}/>
         <TransactionsList txns={this.state.txns}/>
       </div>
     );
--- src/components/AddTransactionForm.js
+++ src/components/AddTransactionForm.js
@@ -1,15 +1,55 @@
 import React, { Component } from "react";
 
 class AddTransactionForm extends Component {
+
+  state = {
+    date: '2020-07-11',
+    description: '',
+    category: '',
+    amount: '',
+  }
+
+  handleChange = (event) => {
+    this.setState({[event.target.name]: event.target.value})
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    this.props.submitForm(this.state)
+      .then(this.setState({
+        date: '2020-07-11',
+        description: '',
+        category: '',
+        amount: '',
+      }))
+  }
+
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form className="ui form" onSubmit={this.handleSubmit}>
           <div className="inline fields">
-            <input type="date" name="date" />
-            <input type="text" name="description" placeholder="Description" />
-            <input type="text" name="category" placeholder="Category" />
+            <input 
+              value={this.state.date} 
+              onChange={this.handleChange}
+              type="date"
+              name="date" 
+            />
+            <input 
+              value={this.state.description} 
+              onChange={this.handleChange}
+              type="text"
+              name="description" placeholder="Description" 
+            />
+            <input 
+              value={this.state.category}
+              onChange={this.handleChange}
+              type="text"
+              name="category" placeholder="Category" 
+            />
             <input
+              value={this.state.amount}
+              onChange={this.handleChange}
               type="number"
               name="amount"
               placeholder="Amount"
