f44072c4258f335895a9becac6c7aef038459a2f
elliott-king <8884571+elliott-king@users.noreply.github.com>
2020-06-11 10:06:29 -0400

    See a table of the transactions

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -3,13 +3,26 @@ import TransactionsList from "./TransactionsList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
+const API = 'http://localhost:6001/transactions'
+
 class AccountContainer extends Component {
+
+  state = {
+    txns: []
+  }
+
+  componentDidMount() {
+    fetch(API)
+      .then(res => res.json())
+      .then(txns => this.setState({txns: txns}))
+  }
+
   render() {
     return (
       <div>
         <Search />
         <AddTransactionForm />
-        <TransactionsList />
+        <TransactionsList txns={this.state.txns}/>
       </div>
     );
   }
--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-const Transaction = () => {
+const Transaction = (props) => {
   return (
     <tr>
-      <td>{"your code here..."}</td>
-      <td>{"your code here..."}</td>
-      <td>{"your code here..."}</td>
-      <td>{"your code here..."}</td>
+      <td>{props.txn.date}</td>
+      <td>{props.txn.description}</td>
+      <td>{props.txn.category}</td>
+      <td>{props.txn.amount}</td>
     </tr>
   );
 };
