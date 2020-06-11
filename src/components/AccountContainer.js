314f6ed3f39687b4c7c2f15b7596b3b23e4fb0a3
github username <github email address>
2020-06-11 10:36:30 -0400

    Rendered and working on forms

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -3,13 +3,47 @@ import TransactionsList from "./TransactionsList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
 
+const baseUrl = `http://localhost:6002/transactions`
+
 class AccountContainer extends Component {
+
+  state = {
+    transactions: [],
+    transactionInfo: {
+      date: "2020-06-10",
+      description: 'testing',
+      category: 'Food',
+      amount: '999'
+    }
+  }
+
+  handleForm = (transactionInfo) => {
+    this.setState({
+     
+    })
+  }
+
+  componentDidMount(){
+    fetch(baseUrl)
+    .then(resp => resp.json())
+    .then(transactions => {
+      this.setState({
+        transactions: transactions
+      })
+    })
+  }
+
   render() {
     return (
       <div>
         <Search />
-        <AddTransactionForm />
-        <TransactionsList />
+        <AddTransactionForm 
+          transactionInfo={this.state.transactionInfo} 
+          handleForm={this.handleForm}
+        />
+        <TransactionsList 
+          transactions={this.state.transactions} 
+        />
       </div>
     );
   }
--- src/components/AddTransactionForm.js
+++ src/components/AddTransactionForm.js
@@ -1,19 +1,45 @@
 import React, { Component } from "react";
 
 class AddTransactionForm extends Component {
+
+  handleChange = (evt) => {
+    this.props.handleForm(this.props)
+    
+  }
+
   render() {
+    
     return (
       <div className="ui segment">
         <form className="ui form">
           <div className="inline fields">
-            <input type="date" name="date" />
-            <input type="text" name="description" placeholder="Description" />
-            <input type="text" name="category" placeholder="Category" />
+            <input 
+              type="date" 
+              name="date" 
+              value={this.props.transactionForm.date}
+              onChange={this.handleChange} 
+            />
+            <input 
+              type="text" 
+              name="description" 
+              value={this.props.transactionForm.description} 
+              placeholder="Description" 
+              onChange={this.handleChange} 
+            />
+            <input 
+              type="text" 
+              name="category" 
+              value={this.props.transactionForm.category} 
+              placeholder="Category" 
+              onChange={this.handleChange} 
+            />
             <input
               type="number"
               name="amount"
               placeholder="Amount"
+              value={this.props.transactionForm.amount}
               step="0.01"
+              onChange={this.handleChange} 
             />
           </div>
           <button className="ui button" type="submit">
