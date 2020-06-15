cfa46c6ff391b5d9379ffc1d7255db219f1f2114
github username <github email address>
2020-06-15 13:59:11 -0400

    post is not working

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -8,10 +8,12 @@ import AddTransactionForm from "./AddTransactionForm";
 class AccountContainer extends Component {
 
   state = {
-    trans: [],
-    tranInForm: {
-      
-    }
+    trans: [], 
+      id: null,
+      date: '',
+      description: '',
+      category: '',
+      amount: ''
   }
 
   componentDidMount() {
@@ -22,11 +24,55 @@ class AccountContainer extends Component {
     })
   }
 
+  addToTransactions = newTran => {
+    this.setState({ trans: [
+      ...this.state.trans, newTran
+    ]})
+  }
+
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+    event.persist()
+
+    const newTransaction = {
+      id: this.state.id,
+      date: this.state.date,
+      description: this.state.description,
+      category: this.state.category,
+      amount: this.state.amount
+    }
+
+    fetch('http://localhost:6001/transactions', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify(...this.state.trans, newTransaction)
+    })
+      .then(res => res.json())
+      .then(newTran => this.addToTransactions(newTran))
+  }
+
   render() {
+    console.log(this.state)
     return (
       <div>
         <Search />
-        <AddTransactionForm />
+        {/* <AddTransactionForm tranInForm={this.state.tranInForm} handleChange={this.handleChange} /> */}
+        <AddTransactionForm 
+        id={this.state.id} 
+        date={this.state.date} 
+        description={this.state.description} 
+        category={this.state.category}
+        amount={this.state.amount}
+        handleChange={this.handleChange}
+        handleSubmit={this.handleSubmit} />
+
         <TransactionsList trans={this.state.trans} />
       </div>
     );
--- src/components/AddTransactionForm.js
+++ src/components/AddTransactionForm.js
@@ -1,15 +1,19 @@
 import React, { Component } from "react";
 
 class AddTransactionForm extends Component {
+
   render() {
+
+    const { id, date, description, category, amount, handleChange, handleSubmit } = this.props 
+
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form className="ui form"onSubmit={handleSubmit} >
           <div className="inline fields">
-            <input type="date" name="date" />
-            <input type="text" name="description" placeholder="Description" />
-            <input type="text" name="category" placeholder="Category" />
-            <input type="number" name="amount" placeholder="Amount" step="0.01" />
+            <input type="date" name="date" value={date} onChange={handleChange}/>
+            <input type="text" name="description" placeholder="Description" value={description} onChange={handleChange}/>
+            <input type="text" name="category" placeholder="Category" value={category} onChange={handleChange}/>
+            <input type="number" name="amount" placeholder="Amount" step="0.01" value={amount} onChange={handleChange}/>
           </div>
           <button className="ui button" type="submit">
             Add Transaction
